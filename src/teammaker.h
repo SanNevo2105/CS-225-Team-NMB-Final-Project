17f4a99d735eb488dc1a6deb4405bf63002f88ec
Noby <ThachakritK@gmail.com>
2022-11-11 16:51:59 -0600

    Update teammaker.h

--- src/teammaker.h
+++ src/teammaker.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
 
+class TeamMaker{
+  public:
+    TeamMaker(const std::string& teamFile, const std::string& indivFile);
+    std::map<std::string, std::map<std::string, double>> teammateParser(const std::string& fileName);
+    std::map<std::string, double> usageParser(const std::string& fileName);
+    ~TeamMaker();
+    bfs(const std::string name);
+    floydWarshall();
+    
+  
+  Private:
+};
