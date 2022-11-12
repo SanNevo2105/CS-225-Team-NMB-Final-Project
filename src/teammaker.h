fe278ffb6b7586c987bda816299c45fbfbab1352
Noby <ThachakritK@gmail.com>
2022-11-11 19:01:06 -0600

    Update teammaker.h

--- src/teammaker.h
+++ src/teammaker.h
@@ -8,17 +8,23 @@ class TeamMaker{
   public:
     //constructor taking in the teammate percentage data and individual usage percentage data
     TeamMaker(const std::string& teamFile, const std::string& usageFile);
+  
     //parser for teammates data set, return a vector of tuple containing Pokemon name, 
     //map of teammate and its percentage, and viability ceiling
     std::vector<std::tuple<std::string, std::map<std::string, double>, double>> teammatesParser(const std::string& fileName);
+  
     //parser for usage data set, returns a map of Pokemon name and its usage percentage
     std::map<std::string, double> usageParser(const std::string& fileName);
+  
     //destructor
     ~TeamMaker();
+  
     //dfs for finding whether a pokemon is connected to another one or not, if not, then the team sucks
     bool dfs(const std::string start, const std::string key);
+  
     //floyd warshall for finding the weight between each pokemon
     std::vector<std::vector<double>> floydWarshall();
+  
     //the function for generating the team, taking in a string of pokemon names separated by comma and space
     //use helper function pokemonsToVector to turn the string of pokemons into a vector of pokemons
     std::vector<std::string> generateTeam(const std::string& pokemons);
@@ -26,12 +32,16 @@ class TeamMaker{
   private:
     //map mapping pokemon with its index
     std::map<std::string, unsigned> index_;
+  
     //vector of its usage percentage, with index representing a pokemon
     std::vector<double> usage_;
+  
     //vector representing map of index of pokemon and its teammates percentage
     std::vector<std::map<unsigned, double>> teammates_;
+  
     //vector representing the viability ceiling of each pokemon
     std::vector<double> viability_;
+  
     //helper function for generateTeam for turning a string of pokemon names to a vector of pokemons
     std::vector<string> pokemonsToVector(const std::string& pokemons);
 };
