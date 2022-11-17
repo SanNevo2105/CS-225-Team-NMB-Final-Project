d5f705286543025f487d3f40131e165551afcb51
Noby <ThachakritK@gmail.com>
2022-11-17 03:56:18 +0000

    testing out make, failed

---
+++ build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/benchmark/detail/catch_stats.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/benchmark/detail/catch_stats.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/benchmark/internal/catch_benchmark_combined_tu.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/benchmark/internal/catch_benchmark_combined_tu.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_approx.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_approx.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_assertion_result.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_assertion_result.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_config.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_config.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_message.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_message.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_registry_hub.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_registry_hub.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_session.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_session.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_test_case_info.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_test_case_info.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_test_spec.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_test_spec.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_timer.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_timer.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_tostring.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_tostring.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_totals.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_totals.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/catch_version.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/catch_version.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/generators/internal/catch_generators_combined_tu.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/generators/internal/catch_generators_combined_tu.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/interfaces/catch_interfaces_combined_tu.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/interfaces/catch_interfaces_combined_tu.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/interfaces/catch_interfaces_reporter.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/interfaces/catch_interfaces_reporter.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_assertion_handler.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_assertion_handler.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_clara.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_clara.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_combined_tu.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_combined_tu.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_commandline.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_commandline.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_common.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_common.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_console_colour.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_console_colour.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_context.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_context.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_debug_console.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_debug_console.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_debugger.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_debugger.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_enforce.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_enforce.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_enum_values_registry.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_enum_values_registry.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_exception_translator_registry.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_exception_translator_registry.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_fatal_condition_handler.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_fatal_condition_handler.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_list.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_list.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_output_redirect.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_output_redirect.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_random_number_generator.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_random_number_generator.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_reporter_registry.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_reporter_registry.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_run_context.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_run_context.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_section.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_section.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_singletons.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_singletons.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_stream.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_stream.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_string_manip.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_string_manip.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_stringref.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_stringref.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_tag_alias_registry.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_tag_alias_registry.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_test_case_registry_impl.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_test_case_registry_impl.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_test_case_tracker.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_test_case_tracker.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_test_registry.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_test_registry.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_test_spec_parser.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_test_spec_parser.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_textflow.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_textflow.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_wildcard_pattern.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_wildcard_pattern.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/internal/catch_xmlwriter.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_xmlwriter.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/matchers/catch_matchers_floating.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/matchers/catch_matchers_floating.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/matchers/catch_matchers_string.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/matchers/catch_matchers_string.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/matchers/catch_matchers_templated.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/matchers/catch_matchers_templated.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/matchers/internal/catch_matchers_combined_tu.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/matchers/internal/catch_matchers_combined_tu.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_automake.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_automake.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_combined_tu.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_combined_tu.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_compact.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_compact.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_console.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_console.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_cumulative_base.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_cumulative_base.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_junit.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_junit.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_listening.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_listening.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_sonarqube.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_sonarqube.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_streaming_base.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_streaming_base.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_tap.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_tap.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_teamcity.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_teamcity.cpp.o"
+  "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-src/src/catch2/reporters/catch_reporter_xml.cpp" "/workspaces/CS-225-Team-NMB-Final-Project/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/reporters/catch_reporter_xml.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/catch2-src/src/catch2/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/teammaker.cpp
+++ src/teammaker.cpp
@@ -1,22 +1,56 @@
 #include "teammaker.h"
 #include <fstream>
+#include <iterator>
 
 TeamMaker::TeamMaker(const std::string& teamFile, const std::string& usageFile){}
 
 std::vector<std::tuple<std::string, std::map<std::string, double>, double>> TeamMaker::teammatesParser(const std::string& fileName){
-    std::ifstream words(fileName);
-
-    if (words.is_open()){
-
+    std::ifstream textFile(fileName);
+    std::string line;
+    std::vector<std::tuple<std::string, std::map<std::string, double>, double>> result;
+    unsigned plusCount = 0;
+    if (textFile.is_open()){
+        std::string name;
+        double viabilityCeiling;
+        while (getline(textFile, line)) {
+            if (line[0] == '+') {
+                plusCount++;
+                if (plusCount%9 == 1) {
+                    getline(textFile, line);
+                    unsigned i = 0;
+                    while (line[i] == '|' || line[i] == ' ') {
+                        i++;
+                    }
+                    while (line[i] != '|') {
+                        name += line[i];
+                        i++;
+                    }
+                    unsigned j = name.size();
+                    while (name[j-1] == ' ') {
+                        j--;
+                    }
+                    name = name.substr(0, j);
+                }
+            }
+        }
     }
+    return result;
 }
 
-std::map<std::string, double> TeamMaker::usageParser(const std::string& fileName){}
+std::map<std::string, double> TeamMaker::usageParser(const std::string& fileName){
+    return std::map<std::string, double>();
+}
 
 TeamMaker::~TeamMaker(){}
 
-bool TeamMaker::dfs(const std::string start, const std::string key){}
+bool TeamMaker::dfs(const std::string start, const std::string key){
+    return true;
+}
 
-std::vector<std::vector<double>> TeamMaker::floydWarshall(){}
+std::vector<std::vector<double>> TeamMaker::floydWarshall(){
+    return std::vector<std::vector<double>>();
+}
 
-std::vector<std::string> TeamMaker::generateTeam(const std::string& pokemons){}
+std::vector<std::string> TeamMaker::generateTeam(const std::string& pokemons){
+    return std::vector<std::string>();
+}
