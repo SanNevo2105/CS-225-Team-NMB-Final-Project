d56d545ef7e2be3c6e61440d921fdcec97f4467a
Ming <93100018+89310637ming@users.noreply.github.com>
2022-11-26 22:17:08 +0000

    constructor

--- src/teammaker.cpp
+++ src/teammaker.cpp
@@ -5,7 +5,69 @@
 #include <iostream>
 #include <stack>
 
-TeamMaker::TeamMaker(const std::string& teamFile, const std::string& usageFile){}
+using namespace std; 
+
+TeamMaker::TeamMaker(const std::string& teamFile, const std::string& usageFile){
+
+std::vector<std::tuple<std::string, std::map<std::string, double>, double>> result = teammatesParser(teamFile);
+
+//string corresponds to the get<0> result.at(i);
+//unsinged is just from 0 to vector.size();
+for(int i = 0; i < result.size(); i++) {
+    // populate the index_
+    index_[get<0> (result.at(i))] =  i;
+}
+// populating the teammates_ private variable
+// std::vector<std::map<unsigned, double>> teammates_;
+// the unsinged in the map of teammates correponds to the string(key) in the map of index
+// converting string to unsinged --- use stoul
+// std::vector<std::string> key_in_index;
+// for (std::map<std::string, unsigned>::iterator it=index_.begin() ; it != index_.end(); ++it )
+// {
+// key_in_index.push_back(index_.first)
+// }
+
+// std::vector<unsigned int> teammates_key;
+// for(unsigned int i = 0; i < key_in_index.size(); i++) {
+//     unsigned int x = std::stoul(key_in_index.at(i), nullptr, 0);
+//     teammates_key.push_back(x);
+// }
+
+// getting the double from the map in result
+// goes through the result vector, then go to the tuples position, then go to the map and obtain the info
+std::vector<double> value_in_teammate;
+
+// loop through the vector, then get the tuple position, then get the map, insert the map into a new vector of map
+// get the value from the vector of map
+
+for(int i = 0; i < result.size(); i++) {
+    // gets the tuple position and then the value in the map
+    value_in_teammate.push_back(get<1>(result.at(i)).second);
+}
+// map is a vector of pairs  
+
+
+// now I have all the info I need to populate teammates_
+
+std::map<unsigned, double> inner;
+for(int i = 0; i < value_in_teammate; i++) {
+    inner.insert(i,value_in_teammate.at(i));
+    teammates_.push_back(inner);
+}
+
+// populating the viability 
+for(int i = 0; i < result.size(); i++) {
+viability_.push_back(get<2> result.at(i));
+}
+
+// populating usage
+std::map<std::string, double> match = usageParser(usageFile);
+// iterates through the map, then push the value into private variable usage
+for (std::map<std::string, double>::iterator it=match.begin() ; it != match.end(); ++it )
+{
+usage.push_back(it.second);
+}
+}
 
 std::vector<std::tuple<std::string, std::map<std::string, double>, double>> TeamMaker::teammatesParser(const std::string& fileName){
     std::ifstream textFile(fileName);
