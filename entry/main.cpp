8640c2279f7ae80c2499d4d15e28bca0537b3f54
Ming <93100018+89310637ming@users.noreply.github.com>
2022-11-27 01:42:57 +0000

    construcor finished

--- entry/main.cpp
+++ entry/main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
     std::cout << "ree" << std::endl;
-    TeamMaker* tm = new TeamMaker("die", "die");
+    TeamMaker* tm = new TeamMaker("/workspaces/CS-225-Team-NMB-Final-Project/gen8ou-1825.txt", "/workspaces/CS-225-Team-NMB-Final-Project/dataset.csv"); 
     std::vector<std::tuple<std::string, std::map<std::string, double>, double>> teammates = tm->teammatesParser("/workspaces/CS-225-Team-NMB-Final-Project/gen8ou-1825.txt");
     for (auto entry:teammates) {
         std::string name = std::get<0>(entry);
@@ -14,14 +14,13 @@ int main(){
             std::cout<< "   " << t.first << ": " << t.second <<std::endl;
         }
     }
-    
-    TeamMaker* team = new TeamMaker("die", "die");
+    std::cout << "usageParser testing" << std::endl;
+    TeamMaker* team = new TeamMaker("/workspaces/CS-225-Team-NMB-Final-Project/gen8ou-1825.txt", "/workspaces/CS-225-Team-NMB-Final-Project/dataset.csv"); 
     std::map<std::string, double> individual = team->usageParser("/workspaces/CS-225-Team-NMB-Final-Project/dataset.csv");
     for(auto entry:individual){
         std::string name = std::get<0>(entry);
         double vc = std::get<1>(entry);
         std::cout<< name << " " << vc <<std::endl;
     }
-    
     return 1;
 }
