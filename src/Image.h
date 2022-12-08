bd40ba91f9074bdb2e560c95de2686f2b39d7e8b
wushangfuguang <86706396+wushangfuguang@users.noreply.github.com>
2022-12-07 18:21:07 -0800

    Update Image.h

--- src/Image.h
+++ src/Image.h
@@ -1,1 +1,35 @@
+/**
+ * @file Image.h
+ * Contains your declaration of the interface for the Image class.
+ */
 
+#pragma once
+
+#include <string>
+using std::string;
+
+//#include"/workspaces/CS225/release-f22-main/mp_stickers/lib/cs225/PNG.h"   //when grading on pl, what should this be?
+//#include"PNG.h"
+#include"cs225/PNG.h"
+
+namespace cs225 {
+    class Image : public PNG{
+        public:
+            void darken();
+            void darken(double amount);
+            void desaturate();
+            void desaturate(double amount);
+            void grayscale();
+            void illinify();
+            void lighten();
+            void lighten(double amount);
+            void rotateColor(double degrees);
+            void saturate();
+            void saturate(double amount);
+            void scale(double factor);
+            void scale(unsigned w, unsigned h);
+
+        private:
+        //nothing in private
+    };
+}
